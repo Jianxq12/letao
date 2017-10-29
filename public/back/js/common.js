50317f5b0f362595520f9bf4b3521ec7f1a67adf
Jianxq12 <wenmeng12@126.com>
2017-10-29 21:45:47 +0800

    登录功能完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,3 +1,26 @@
-/**
- * Created by Administrator on 2017/10/29 0029.
- */
+
+// 检测用户是否登录
+// if(location.href.indexOf('login.html')<0){
+//     $.ajax({
+//         url:'/employee/checkRootLogin',
+//         type:'get',
+//         success:function (data) {
+//             console.log(data);
+//             if(data.error==400){
+//                 location.href = 'login.html';
+//             }
+//         }
+//     })
+// }
+
+$(document).ajaxStart(function () {
+    // console.log('呵呵');
+    NProgress.start();
+})
+
+$(document).ajaxStop(function () {
+    setTimeout(function () {
+        // console.log('hehe');
+        NProgress.done();
+    },1000)
+})
