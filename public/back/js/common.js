ab54171b30081dfedf56608d4431316e822a7389
Jianxq12 <wenmeng12@126.com>
2017-10-30 09:10:34 +0800

    侧边栏和退出功能完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,17 +1,17 @@
 
 // 检测用户是否登录
-// if(location.href.indexOf('login.html')<0){
-//     $.ajax({
-//         url:'/employee/checkRootLogin',
-//         type:'get',
-//         success:function (data) {
-//             console.log(data);
-//             if(data.error==400){
-//                 location.href = 'login.html';
-//             }
-//         }
-//     })
-// }
+if(location.href.indexOf('login.html')<0){
+    $.ajax({
+        url:'/employee/checkRootLogin',
+        type:'get',
+        success:function (data) {
+            console.log(data);
+            if(data.error==400){
+                location.href = 'login.html';
+            }
+        }
+    })
+}
 
 $(document).ajaxStart(function () {
     // console.log('呵呵');
@@ -23,4 +23,32 @@ $(document).ajaxStop(function () {
         // console.log('hehe');
         NProgress.done();
     },1000)
+})
+
+// 点击分类管理，显示或隐藏二级分类
+$('.child').prev().on('click',function(){
+	$(this).next().slideToggle();
+})
+
+// 点击菜单，显示或隐藏侧边栏
+$('.icon_menu').on('click',function(){
+	$('.lt_aside').toggleClass('now');
+	$('.lt_main').toggleClass('now');
+})
+
+// 共用的退出功能
+$('.icon_logout').on('click',function(){
+	$('#logoutModal').modal('show');
+})
+
+$('.btn_logout').on('click',function(){
+	$.ajax({
+		url:'/employee/employeeLogout',
+		type:'get',
+		success:function(data){
+			if(data.success){
+				location.href = 'login.html';
+			}
+		}
+	})
 })
